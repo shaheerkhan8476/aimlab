9645ed79163735949e45cfcf7c196081071e0c45
Shaheer Khan <119394707+shaheerkhan8476@users.noreply.github.com>
2025-01-26 16:52:56 -0500

    Created User Table/Model to add User to Database

--- backend/routes/supabase/routes.go
+++ backend/routes/supabase/routes.go
@@ -6,7 +6,9 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/google/uuid"
 	supabase "github.com/nedpals/supabase-go"
+	model "gitlab.msu.edu/team-corewell-2025/models"
 )
 
 var Supabase *supabase.Client
@@ -17,23 +19,33 @@ func InitClient(url, key string) *supabase.Client {
 }
 
 func SignUpUser(w http.ResponseWriter, r *http.Request) {
-	var reqBody struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
+	var userRequest UserCreateRequest
 	bodyBytes, _ := io.ReadAll(r.Body)
-	err := json.Unmarshal(bodyBytes, &reqBody)
+	err := json.Unmarshal(bodyBytes, &userRequest)
 	if err != nil {
 		print(err)
 	}
 	ctx := context.Background()
 	user, err := Supabase.Auth.SignUp(ctx, supabase.UserCredentials{
-		Email:    reqBody.Email,
-		Password: reqBody.Password,
+		Email:    userRequest.Email,
+		Password: userRequest.Password,
 	})
-
 	if err != nil {
-		panic(err)
+		print(err)
+	}
+	parsedID, err := uuid.Parse(user.ID)
+	if err != nil {
+		print(err)
+	}
+	newUser := model.User{
+		Id:      parsedID,
+		Name:    userRequest.Name,
+		Email:   userRequest.Email,
+		IsAdmin: false,
+	}
+	err = Supabase.DB.From("users").Insert(newUser).Execute(nil)
+	if err != nil {
+		print(err)
 	}
 	b, err := json.Marshal(user)
 	if err != nil {
