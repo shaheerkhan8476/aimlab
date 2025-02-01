7fb98af013f3dc39affb4b7ebf3e9025b447a2ab
Christopher Nguyen <45804616+ToffeeNTea@users.noreply.github.com>
2025-02-01 16:32:30 -0500

    Rearranged patients model

---
+++ backend/models/patient.go
@@ -0,0 +1,19 @@
+package model
+
+import "time"
+
+type Patient struct {
+	ID                   int       `json:"id"`
+	Name                 string    `json:"name"`
+	DateOfBirth          time.Time `json:"date_of_birth"`
+	Age                  int       `json:"age"`
+	Gender               string    `json:"gender"`
+	MedicalCondition     *string   `json:"medical_condition"`
+	MedicalHistory       *string   `json:"medical_history"`
+	FamilyMedicalHistory *string   `json:"family_medical_history"`
+	SurgicalHistory      *string   `json:"surgical_history"`
+	Cholesterol          *string   `json:"cholesterol"`
+	Allergies            *string   `json:"allergies"`
+	Medications          *string   `json:"medications"`
+	PatientMessage       *string   `json:"patient_message"`
+}
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,8 +1,6 @@
 package model
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
@@ -11,20 +9,4 @@ type User struct {
 	Name    string    `json:"name"`
 	Email   string    `json:"email"`
 	IsAdmin bool      `json:"isAdmin"`
-}
-
-type Patient struct {
-	ID                   int       `json:"id"`
-	Name                 string    `json:"name"`
-	DateOfBirth          time.Time `json:"date_of_birth"`
-	Age                  int       `json:"age"`
-	Gender               string    `json:"gender"`
-	MedicalCondition     *string   `json:"medical_condition"`
-	MedicalHistory       *string   `json:"medical_history"`
-	FamilyMedicalHistory *string   `json:"family_medical_history"`
-	SurgicalHistory      *string   `json:"surgical_history"`
-	Cholesterol          *string   `json:"cholesterol"`
-	Allergies            *string   `json:"allergies"`
-	Medications          *string   `json:"medications"`
-	PatientMessage       *string   `json:"patient_message"`
 }
