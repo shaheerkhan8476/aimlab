a235f2fd40456b9d7375327beee824fee042e91d
Julian Akkashian <akkashi5@msu.edu>
2025-02-03 21:39:18 -0500

    commit for testing if initial_token gets me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import CreateUser from './CreateUser';
 import SignInUser from './SignInUser';
 
-
+//Main App entry point
+//I am not doing a phony comment commit to cheese contribution score.
+//I am testing a legitimate issue with git code ownership.
 function App() {
   return (
     <div>
