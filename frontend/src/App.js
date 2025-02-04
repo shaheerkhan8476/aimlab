f1cec35dffb5265ac0312d58a8a1701f61bee0fb
Julian Akkashian <akkashi5@msu.edu>
2025-02-03 21:44:24 -0500

    please don't get me initial_token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import SignInUser from './SignInUser';
 //Main App entry point
 //I am not doing a phony comment commit to cheese contribution score.
 //I am testing a legitimate issue with git code ownership.
+//The initial_token did indeed get me.
+//Pushing again to see if Personal Access TOken fixes.
 function App() {
   return (
     <div>
