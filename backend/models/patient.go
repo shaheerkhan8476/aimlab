635734eba43a6a8044940ccbf73a357c40264dfb
nguy1110 <nguy1110@msu.edu>
2025-02-04 18:06:37 -0500

    Change ID in patient model to UUID
    Also changed the tables in supabase

--- backend/models/patient.go
+++ backend/models/patient.go
@@ -1,19 +1,21 @@
 package model
 
+import "github.com/google/uuid"
+
 type Patient struct {
-	ID                   int    `json:"id"`
-	Name                 string `json:"name"`
-	DateOfBirth          string `json:"date_of_birth"`
-	Age                  int    `json:"age"`
-	Gender               string `json:"gender"`
-	MedicalCondition     string `json:"medical_condition"`
-	MedicalHistory       string `json:"medical_history"`
-	FamilyMedicalHistory string `json:"family_medical_history"`
-	SurgicalHistory      string `json:"surgical_history"`
-	Cholesterol          string `json:"cholesterol"`
-	Allergies            string `json:"allergies"`
-	Medications          string `json:"medications"`
-	PatientMessage       string `json:"patient_message"`
+	ID                   uuid.UUID `json:"id"`
+	Name                 string    `json:"name"`
+	DateOfBirth          string    `json:"date_of_birth"`
+	Age                  int       `json:"age"`
+	Gender               string    `json:"gender"`
+	MedicalCondition     string    `json:"medical_condition"`
+	MedicalHistory       string    `json:"medical_history"`
+	FamilyMedicalHistory string    `json:"family_medical_history"`
+	SurgicalHistory      string    `json:"surgical_history"`
+	Cholesterol          string    `json:"cholesterol"`
+	Allergies            string    `json:"allergies"`
+	Medications          string    `json:"medications"`
+	PatientMessage       string    `json:"patient_message"`
 }
 
 type Prescription struct {
