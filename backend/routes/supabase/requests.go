0360501dec12fac130c5cdf42f91faa423270371
Shaheer <khansh11@msu.edu>
2025-02-03 21:12:49 -0500

    Added model comments

--- backend/routes/supabase/requests.go
+++ backend/routes/supabase/requests.go
@@ -1,12 +1,13 @@
 package supabase
 
+// User Create Request Model
 type UserCreateRequest struct {
 	Name     string `json:"name"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
 	IsAdmin  bool   `json:"isAdmin"`
 }
-
+// User Login Request Model
 type UserLoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
