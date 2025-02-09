ca3689dd6b1bd41321c4f7bfbc0ef1166557b7cf
Shaheer <khansh11@msu.edu>
2025-02-08 20:08:33 -0500

    Updated results & prescriptions table to use uuid instead of regular id

--- backend/models/supabase.go
+++ backend/models/supabase.go
@@ -18,16 +18,15 @@ type Patient struct {
 }
 
 type Prescription struct {
-	ID            int       `json:"id"`
+	ID            uuid.UUID `json:"id"`
 	Patient_id    uuid.UUID `json:"patient_id"`
-	Name          string    `json:"name"`
 	Medication    string    `json:"medication"`
 	Dose          string    `json:"dose"`
 	Refill_status string    `json:"refill_status"`
 }
 
 type Result struct {
-	ID          int             `json:"id"`
+	ID          uuid.UUID       `json:"id"`
 	Patient_id  uuid.UUID       `json:"patient_id"`
 	Test_name   string          `json:"test_name"`
 	Test_date   string          `json:"test_date"`
