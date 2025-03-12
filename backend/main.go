c2e6e623fac150af2918ed3b35cf3e1720031661
Julian Akkashian <akkashi5@msu.edu>
2025-03-12 10:50:58 -0400

    stu dashboard shows only assigned and incomplete tasks now

--- backend/main.go
+++ backend/main.go
@@ -82,7 +82,8 @@ func main() {
 
 	// Endpoints for tasks (generating, getting, completing, etc.)
 	m.HandleFunc("/generateTasks", supabase.GenerateTasksHTMLWrapper).Methods("POST")
-	m.HandleFunc("/{student_id}/tasks", supabase.GetTasksByStudentID).Methods("GET")
+	m.HandleFunc("/{student_id}/tasks", supabase.GetTasksByStudentID).Methods("POST", "OPTIONS") //had to make this post bc the function expects a body
+	//hardcoded body to show incomplete tasks ^^^
 	m.HandleFunc("/{student_id}/tasks/week", supabase.GetTaskByWeek).Methods("GET")
 	m.HandleFunc("/{student_id}/tasks/{task_id}", supabase.GetTaskByID).Methods("GET")
 	m.HandleFunc("/{student_id}/tasks/{task_id}/completeTask", supabase.CompleteTask).Methods("POST")
