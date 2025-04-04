2c8ef4958a33f892ac09ed243092e847e3c572ff
Isaac <zelenaki@msu.edu>
2025-04-04 13:01:39 -0400

    add comments and remove clerkship option

--- frontend/src/pages/ResetPassword.js
+++ frontend/src/pages/ResetPassword.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import "./css/Login.css";
+import "./css/Login.css";//My Provided Style
 function ResetPassword()
 {
     //Create blank form for data user enters
@@ -9,8 +9,8 @@ function ResetPassword()
         password: '',
         token: ''
     });
-    const [message, setMessage] = useState(""); 
-    const navigate = useNavigate();
+    const [message, setMessage] = useState(""); //sets the message to display
+    const navigate = useNavigate();//used to navigate back to login
 
     //gets acess token from url with hash
     useEffect(() => {
